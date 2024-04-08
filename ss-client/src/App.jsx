fc05df92c17a15e21c0db9be801de75820ab7805
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-04-08 22:15:51 +0100

    add tailwindcss to frontend
    - installed tailwindcss and required dependencies
    - added tailwind stlyes to index and main app pages
    - other minor frontend updates

--- ss-client/src/App.jsx
+++ ss-client/src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-    return <h1>Hello World! We&apos;re ShiftSphere</h1>
+    return (
+        <h1 className="text-8xl font-bold text-white">
+            Hello World! We&apos;re{" "}
+            <span className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-transparent bg-clip-text">
+                ShiftSphere
+            </span>
+        </h1>
+    );
 }
 
 export default App;
