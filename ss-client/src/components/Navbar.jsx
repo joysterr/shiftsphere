ae5665302f22a683142a0a0cea96b9945e8d521f
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-04-11 14:14:25 +0100

    update eslint config to prevent conflict with prettier
    - installed eslint pritter config
    - updated all react files with proper lint rules and formatting

--- ss-client/src/components/Navbar.jsx
+++ ss-client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 import navItems from "../utils/navItems";
 
 function Navbar() {
